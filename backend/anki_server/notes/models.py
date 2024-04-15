0035d0805afe9c937f0875b1f976693903b4565a
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2024-04-14 23:57:08 -0600

    added Tag and Note models

--- backend/anki_server/notes/models.py
+++ backend/anki_server/notes/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from mdeditor.fields import MDTextField
 
-# class Blog(models.Model):
 
-#     body = MDTextField(null=True, blank=True)
+class Note(models.Model):
+    title : models.CharField(max_length=255)
+    content : models.TextField()
+    tags : models.ManyToManyField(Tag)
+    lastEdited : models.DateTimeField()
+    dateCreated : models.DateTimeField()
+    linkedTo :  models.ManyToManyField(Note)
+    linkedFrom : models.MayToManyField(Note)
 
+class Tag(models.Model):
+    name : models.CharField()
