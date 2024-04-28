f6a69237e10ab02cb28c5ced3d13aa9cb700e7b7
Cervantes Duarte Jose Fernando <licfernandoc@ciencias.unam.mx>
2024-04-28 16:51:20 -0600

    cree la carpeta static para meter las imagenes y se puedan renderizar

--- front-end/src/routes/landing/+page.svelte
+++ front-end/src/routes/landing/+page.svelte
@@ -4,7 +4,7 @@
     import "../../bootstrap.min.css"
 	    import NavBar from "../../components/NavBar/NavBar.svelte";
       import Sidebar from "../../components/Sidebar/Sidebar.svelte";
-      
+
       import Footer from "../../components/Footer/Footer.svelte";
   </script>
 
