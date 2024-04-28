13361ef6f25bf6717cbdfea1bc68fa6a4bb242ed
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2024-04-28 02:58:42 -0600

    refactor

--- backend/anki_server/userapp/apps.py
+++ backend/anki_server/userapp/apps.py
@@ -6,9 +6,3 @@ class UserappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userapp'
     
-#     def ready(self):
-#         post_migrate.connect(create_registro_on_ready, sender=self)
-
-# def create_registro_on_ready(sender, **kwargs):
-#     from userapp.models import get_default_user
-#     get_default_user()
