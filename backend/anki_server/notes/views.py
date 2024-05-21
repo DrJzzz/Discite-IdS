7f0238bb9f5e833030aa9f70b5e8ca76639c01c6
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2024-05-21 00:03:40 -0600

    update note count on create and delete

--- backend/anki_server/notes/views.py
+++ backend/anki_server/notes/views.py
@@ -23,24 +23,21 @@ class NoteViewSet(viewsets.ModelViewSet):
         serializer = NoteHistorySerializer(note, context={'request': request})
         return response.Response(serializer.data, status=status.HTTP_200_OK)
     
-    # @action(detail=True, methods=['GET'])
-    # def get_last_edited(self, request, *args, **kwargs):
-    #     history = self.get_object().history.order_by('history_date')[:1].get()
-    #     serializer = HistoricalRecordsSerializer(history)
-    #     return response.Response(serializer.data, status=status.HTTP_200_OK)
     
-    # @action(detail=True, methods=['GET'])
-    # def get_created(self, request, *args, **kwargs):
-    #     history = self.get_object().history.filter(comment='Created')
-    #     serializer = NoteHistoru
-    #     return response.Response(history, status=status.HTTP_200_OK)
+    def perform_create(self, serializer):
+        note = serializer.save()
+        note.notebook.note_count = note.notebook.note_count + 1
+        note.notebook.save()
+        
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    
 class NotebookViewSet(viewsets.ModelViewSet):
     queryset = Notebook.objects.all()
     serializer_class = NotebookSerializer    
     
     def perform_create(self, serializer):
-       serializer.save(owner=self.request.user)
+        serializer.save(owner=self.request.user)
 
 
 
@@ -49,7 +46,7 @@ class NotebookViewSet(viewsets.ModelViewSet):
 
 def notes_notebook(request, pk):
     notebook = get_object_or_404(Notebook, pk=pk)
-    notes = notebook.notebook_ref.all()
+    notes = notebook.notebook.all()
 
     data = {
         'notebook': {
