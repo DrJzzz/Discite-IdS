b75300f8e3c1893b7e7e66e3841d745777368cbb
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2024-05-27 05:09:34 -0600

    chages NewDeck.svelte

--- front-end/src/components/Forms/NewDeck.svelte
+++ front-end/src/components/Forms/NewDeck.svelte
@@ -70,16 +70,38 @@
         );
     }
 
+    let tag_name = '';
+    async function createTag(){
+        
+        const data = {'name': `${tag_name}`};
+        console.log(JSON.stringify(data))
+        try {
+            const token = localStorage.getItem('key');
+            const csrftoken = getCookie('csrftoken');
+            const response = await fetch('http://127.0.0.1:8000/tags/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`,
+                    'X-CSRFToken': `${csrftoken}`
+                },
+                body: JSON.stringify(data),
+                credentials : 'include',
+            });
 
-    function changeIdHistory(id){
-        id_history = id;
-        HistoryStore.subscribe(cards =>{
-            // Encontrar el objeto con el mismo history_id
-            history = cards.find(card => card.history_id === id);
-        });
+            if (response.ok) {
+                alertSuccess('Added new Tag.');
+                await invalidateAll();
+            } else {
+                alertError('Failed to add new Tag.');
+            }
+        } catch (error) {
+            console.error('An error occurred while submitting the form:', error);
+            alertError('An error occurred while adding new deck.');
+        }
     }
 
-    let tag_name = '';
+    
 </script>
 
 <div>
