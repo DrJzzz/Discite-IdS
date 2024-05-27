1e25ca885b8aa9ed36f8dab56b5a95abec150289
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2024-05-27 04:59:41 -0600

    fixed user public preview

--- backend/anki_server/userapp/views.py
+++ backend/anki_server/userapp/views.py
@@ -88,14 +88,21 @@ class UserViewSet(viewsets.ModelViewSet):
             if len(shared_decks) == 0 and len(shared_notebooks) == 0:
                 continue
             
-            deck_tags = list(shared_decks.values_list('tags'))
-            notebook_tags = list(shared_notebooks.values( 'tags'))
-            print(deck_tags)
+            #deck_tags = list(shared_decks.values_list('tags'))
+            deck_tags = {'tags' : [x['tags'] for x in shared_decks.values('tags')]}
+            notebook_tags = {'tags' : [x['tags'] for x in shared_notebooks.values('tags')]}
+            
+            decks = list(shared_decks.values('id', 'name','card_count'))
+            decks[0]['tags'] = deck_tags.get('tags')
+            
+            notebooks = list(shared_notebooks.values('id', 'name','note_count'))
+            notebooks[0]['tags'] = notebook_tags.get('tags')
+            
                      
             item = {
                 'user' : {'id': user.id, 'name': user.name, 'email': user.email},
-                'decks' : list(shared_decks.values('id', 'name','card_count', 'tags')),
-                'notebooks' : list(shared_notebooks.values('id', 'name','note_count', 'tags')),
+                'decks' : decks,
+                'notebooks' : notebooks,
             }
             values.append(item)
         
